e4c7632fb86c3f8f3a5795561c8b3bdc839fa719
Michelle Morin <morin2m@gmail.com>
2020-04-27 16:32:57 -0700

    Updates

---
+++ precache-manifest.7e81ea0cf761ce32b83165844f8173a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7dc5d78245d63ad5d40dd3ae7e72197",
+    "url": "/Earth-Merch-Redux/index.html"
+  },
+  {
+    "revision": "8ef1907ab8f8d42421b7",
+    "url": "/Earth-Merch-Redux/static/css/main.f7df75fc.chunk.css"
+  },
+  {
+    "revision": "e1c58935b2e8e008e74e",
+    "url": "/Earth-Merch-Redux/static/js/2.6f0caf2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Earth-Merch-Redux/static/js/2.6f0caf2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef1907ab8f8d42421b7",
+    "url": "/Earth-Merch-Redux/static/js/main.93fde543.chunk.js"
+  },
+  {
+    "revision": "cd624d320aaba75a36ce",
+    "url": "/Earth-Merch-Redux/static/js/runtime-main.6bfd3b9c.js"
+  }
+]);
