bca137f04472245947a1a4b0dee1dd3c0d7ab5e3
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-27 10:01:26 -0700

    Add test for add and edit items
    Co-authored-by: Michelle Morin <morin2m@gmail.com>

--- src/__tests__/reducers/item-list-reducer.test.js
+++ src/__tests__/reducers/item-list-reducer.test.js
@@ -1,7 +1,36 @@
 import itemListReducer from '../../reducers/item-list-reducer';
 
 describe('itemListReducer', () => {
+
+  let action;
+  const itemData = {
+    name: 't-shirt',
+    description: '100% hemp',
+    quantity: 5,
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(itemListReducer({}, { type: null })).toEqual({});
   });
+
+  test('should successfully add new item data to masterItemList', ()=> {
+    const { name, description, quantity, id } = itemData;
+    action = {
+      type: 'ADD_ITEM',
+      name: name,
+      description: description,
+      quantity: quantity,
+      id: id
+    };
+    expect(itemListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        description: description,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
+
 });
